fa6af8449bfb4188a370d8226494c12af9f07afc
Luciano Zangeronimo <zangeronimo@gmail.com>
2023-12-09 10:11:01 -0300

    chore: register DI for timesheet project

--- src/infra/extensions/TimeSheetExtension.ts
+++ src/infra/extensions/TimeSheetExtension.ts
@@ -6,8 +6,18 @@ import {
   ClientDelete,
 } from "@application/usecase/timesheet/client";
 import { Registry } from "../di/Registry";
-import { ClientRepository } from "@infra/repository/timesheet";
+import {
+  ClientRepository,
+  ProjectRepository,
+} from "@infra/repository/timesheet";
 import { DbContext } from "@infra/context";
+import {
+  ProjectGetAll,
+  ProjectGetById,
+  ProjectCreate,
+  ProjectUpdate,
+  ProjectDelete,
+} from "@application/usecase/timesheet/project";
 
 export class TimeSheetExtension {
   static init(dbContext: DbContext) {
@@ -16,6 +26,10 @@ export class TimeSheetExtension {
       "IClientRepository",
       new ClientRepository(dbContext)
     );
+    Registry.getInstance().provide(
+      "IProjectRepository",
+      new ProjectRepository(dbContext)
+    );
 
     // Registry Client useCases
     Registry.getInstance().provide("IClientGetAll", new ClientGetAll());
@@ -23,5 +37,12 @@ export class TimeSheetExtension {
     Registry.getInstance().provide("IClientCreate", new ClientCreate());
     Registry.getInstance().provide("IClientUpdate", new ClientUpdate());
     Registry.getInstance().provide("IClientDelete", new ClientDelete());
+
+    // Registry Project useCases
+    Registry.getInstance().provide("IProjectGetAll", new ProjectGetAll());
+    Registry.getInstance().provide("IProjectGetById", new ProjectGetById());
+    Registry.getInstance().provide("IProjectCreate", new ProjectCreate());
+    Registry.getInstance().provide("IProjectUpdate", new ProjectUpdate());
+    Registry.getInstance().provide("IProjectDelete", new ProjectDelete());
   }
 }
